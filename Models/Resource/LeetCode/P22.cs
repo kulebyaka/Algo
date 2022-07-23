2426c848ac1e55d487746d7043b69d18bbf1db9a
kule <kiril.elizarov@gmail.com>
2022-07-23 17:40:31 +0200

    add P22, update .net version

---
+++ Models/Resource/LeetCode/P22.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace Models.Resource.LeetCode;
+
+public class P22
+{
+	/// <summary>
+	/// Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+	/// </summary>
+	public IList<string> GenerateParenthesis(int n)
+	{
+		IList<string> result = new List<string>();
+		if (n == 0)
+			return result;
+		GenerateParenthesis(n, n, "", result);
+		return result;
+	}
+
+	private void GenerateParenthesis(int leftCounter, int rightCounter, string currentString, IList<string> result)
+	{
+		if (0 == leftCounter && 0 == rightCounter)
+		{
+			result.Add(currentString);
+			return;
+		}
+
+		if (leftCounter > 0)
+		{
+			GenerateParenthesis(leftCounter - 1, rightCounter, currentString + "(", result);
+		}
+
+		if (leftCounter < rightCounter)
+		{
+			GenerateParenthesis(leftCounter, rightCounter - 1, currentString + ")", result);
+		}
+	}
+}
---
+++ Tests/LeetCode/P22TestFixture.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Models.Resource.LeetCode;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace Tests.LeetCode;
+
+//TestFixture for P22
+[TestFixture]
+[Parallelizable(ParallelScope.All)]
+public class P22TestFixture
+{
+	[Test]
+	[TestCase(3, new[] { "((()))", "(()())", "(())()", "()(())", "()()()" }),
+	 TestCase(2, new[] { "()()", "(())" })]
+	public void P22Test(int n, string[] expected)
+	{
+		var p22 = new P22();
+		IList<string> actual = p22.GenerateParenthesis(n);
+		actual.OrderBy(a => a).Should().BeEquivalentTo(expected.OrderBy(a => a));
+	}
+}

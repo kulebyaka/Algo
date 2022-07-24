dbca325450a3b3801142e8fa334bc44151b74e26
Kirill Elizarov <Kirill.Elizarov@kinandcarta.com>
2022-07-24 14:47:37 +0200

    test async delay

---
+++ Tests/TaskDelay.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Tests
+{
+	[TestFixture]
+	public class TaskDelayTestFixture
+	{
+		[Test]
+		public void TestMyAsync()
+		{
+			var t = Task.Run(async delegate
+			{
+				await Task.Delay(TimeSpan.FromSeconds(5));
+				Lalalaa();
+			});
+			t.Wait();
+			Console.WriteLine("TestMyAsync");
+		}
+
+		public async void Lalalaa()
+		{
+			Console.WriteLine("lalala works");
+		}
+	}
+}

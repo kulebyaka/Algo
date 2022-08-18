adf78a716f24b2961e08ac980925abe6728a5f44
kule <kiril.elizarov@gmail.com>
2022-08-18 14:06:43 +0200

    tasks and enumerable tricks

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,13 +1,17 @@
-﻿using Models.Structures.Tree;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace ConsoleApp
+// tasks and enumerable tricks
+IEnumerable<Task> taskStars = new[]
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			int?[] input = {5, 1, 4, null, null, 3, 6};
-			var node = new BinaryTreeNode<int>(input);
-		}
-	}
-}
+	Task.Run(() => Console.Write("*")),
+	Task.Run(() => Console.Write("*")),
+};
+await Task.WhenAll(taskStars);
+Console.WriteLine($"{taskStars.Count()} stars!");
+
+IEnumerable<Task> tasksAmpersands = Enumerable.Range(0, 2).Select(_ => Task.Run(() => Console.Write("&")));
+await Task.WhenAll(tasksAmpersands);
+Console.WriteLine($"{tasksAmpersands.Count()} ampersands!");

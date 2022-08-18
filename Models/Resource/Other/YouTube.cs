68e94868ab4537969b2739910f2f9c4a5ca1dace
kule <kiril.elizarov@gmail.com>
2022-08-18 22:17:00 +0200

    add ModifiedBinarySearch algo for an array that was split, reordered and joined together

---
+++ Models/Resource/Other/YouTube.cs
@@ -0,0 +1,29 @@
+﻿namespace Models.Resource.Other;
+
+public class YouTube
+{
+	// An ordered array is split in two and the parts are rearranged.
+	// Find the split point
+	public int SplitPoint(int[] array)
+	{
+		int key = array[0];
+		int low = 0;
+		int high = array.Length - 1;
+		while (low <= high)
+		{
+			int mid = (low + high) / 2 + 1;
+			if (mid == array.Length)
+				return 0;
+
+			if (array[mid - 1] > array[mid])
+				return mid;
+
+			if (key < array[mid])
+				low = mid;
+			else if (key > array[mid])
+				high = mid;
+		}
+
+		return 0;
+	}
+}
---
+++ Tests/YouTubeTestFixtures.cs
@@ -0,0 +1,19 @@
+﻿using Models.Resource.Other;
+using NUnit.Framework;
+
+namespace Tests;
+
+[TestFixture]
+public class YouTubeTestFixtures
+{
+	[Test]
+	[TestCase(new int[] { 1, 2, 3, -2, -1 }, 3)]
+	[TestCase(new int[] { 1, 2, 3, 4, -1 }, 4)]
+	[TestCase(new int[] { 1, 2, 3, 4 }, 0)]
+	public void FindSplitPointTest(int[] input, int expected)
+	{
+		var youTube = new YouTube();
+		int result = youTube.SplitPoint(input);
+		Assert.AreEqual(expected, result);
+	}
+}

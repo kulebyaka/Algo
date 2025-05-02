9967351cc974e92e71711c1494b74b3ddb5c8863
kule <kiril.elizarov@gmail.com>
2025-05-03 00:16:41 +0200

    169 Majority Element

--- py/_169_majority_element.py
+++ py/_169_majority_element.py
@@ -10,4 +10,25 @@ from typing import List
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        pass
+        n = len(nums)
+        majority = n // 2
+        hashset = dict()
+
+        for num in nums:
+            cur = hashset.get(num)
+            if cur is None:
+                cur = 0
+
+            hashset[num] = cur + 1
+
+            if cur >= majority:
+                return num
+
+        return 0
+
+def main():
+    print(Solution().majorityElement([3,2,3]))
+    print(Solution().majorityElement([1]))
+
+if __name__ == "__main__":
+    main()

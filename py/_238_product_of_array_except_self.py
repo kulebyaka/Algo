6338dc8cf39694be8735eb4e390619e666201e54
kule <kiril.elizarov@gmail.com>
2025-05-05 03:02:48 +0200

    238

--- py/_238_product_of_array_except_self.py
+++ py/_238_product_of_array_except_self.py
@@ -28,5 +28,17 @@ from typing import List
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        # TODO: Implement solution
-        pass
+        n = len(nums)
+        result = [1] * n
+
+        prefix = 1
+        for i in range(n):
+            result[i] = prefix
+            prefix *= nums[i]
+
+        suffix = 1
+        for i in range(n - 1, -1, -1):
+            result[i] *= suffix
+            suffix *= nums[i]
+
+        return result

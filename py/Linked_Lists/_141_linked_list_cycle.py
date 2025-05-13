7324159a7c830679f2201dee82c47943039391e6
kule <kiril.elizarov@gmail.com>
2025-05-13 16:01:20 +0200

    141

--- py/Linked_Lists/_141_linked_list_cycle.py
+++ py/Linked_Lists/_141_linked_list_cycle.py
@@ -5,9 +5,9 @@ Topic: Linked Lists
 
 Problem Description:
 Given head, the head of a linked list, determine if the linked list has a cycle in it.
-There is a cycle in a linked list if there is some node in the list that can be reached again by continuously 
-following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer 
-is connected to. Note that pos is not passed as a parameter.
+There is a cycle in a linked list if there is some node in the list that can be reached
+again by continuously following the next pointer. Internally, pos is used to denote the
+index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
 Return true if there is a cycle in the linked list. Otherwise, return false.
 
 Example 1:
@@ -32,6 +32,8 @@ Constraints:
 
 Follow up: Can you solve it using O(1) (i.e. constant) memory?
 """
+from typing import Optional
+
 
 # Definition for singly-linked list.
 class ListNode:
@@ -40,6 +42,13 @@ class ListNode:
         self.next = None
 
 class Solution:
-    def hasCycle(self, head: ListNode) -> bool:
-        # TODO: Implement solution
-        pass
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if head is None or head == []:
+            return False
+        hashset = set()
+        while head.next:
+            if head.next in hashset:
+                return True
+            hashset.add(head.next)
+            head = head.next
+        return False
